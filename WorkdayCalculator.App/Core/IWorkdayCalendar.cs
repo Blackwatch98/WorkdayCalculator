a63abedb665d059f3b531020d55cf5ed6a95bdcc
Daniel Duczymiński <danielduczyminski@gmail.com>
2026-05-24 13:43:23 +0200

    Added core concept for this task

---
+++ WorkdayCalculator.App/Core/IWorkdayCalendar.cs
@@ -0,0 +1,9 @@
+﻿namespace WorkdayCalculator.App.Core;
+
+public interface IWorkdayCalendar
+{
+    public void SetHoliday(DateTime date);
+    public void SetRecurringHoliday(int month, int day);
+    public void SetWorkdayStartAndStop(int startHours, int startMinutes, int stopHours, int stopMinutes);
+    public DateTime GetWorkdayIncrement(DateTime startDate, decimal incrementInWorkdays);
+}
